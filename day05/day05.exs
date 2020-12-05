e7e261ebda25bcf05846fece60105a910aad2374
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-12-05 17:05:19 +0200

    Improve approach since << 3 == x8

--- day05/day05.exs
+++ day05/day05.exs
@@ -7,32 +7,47 @@ stream = File.stream!("input.txt")
 lines = stream |> Enum.map(&String.trim/1)
 
 defmodule Day5 do
-    def splitPassport(p) do
-        %{
-            "row" => locateDim(p |> String.slice(0..6)),
-            "col" => locateDim(p |> String.slice(7..10))
-        }
-    end
-    def locateDim(d) do
-        d |> String.replace(["F", "B", "L", "R"],
+    def getSeatId(p) do
+        p
+        |> String.replace(["F", "B", "L", "R"],
             fn
                 z when z in ["F", "L"] -> "0"
                 o when o in ["B", "R"] -> "1"
             end
         )
-    end
-    def seatId(s) do
-        {c, _} = Integer.parse(s["col"], 2)
-        {r, _} = Integer.parse(s["row"], 2)
-        c + r * 8
+        |> Integer.parse(2)
+        |> elem(0)
     end
 end
 
-res1 = lines |> Enum.map(fn x -> Day5.seatId(Day5.splitPassport(x)) end) |> Enum.max()
+res1 = lines |> Enum.map(fn l -> Day5.getSeatId(l) end) |> Enum.max()
 IO.inspect(res1)
 
-all_passes = lines |> Enum.map(fn x -> Day5.seatId(Day5.splitPassport(x)) end)
+all_passes = lines |> Enum.map(fn l -> Day5.getSeatId(l) end)
 min = all_passes |> Enum.min()
 max = all_passes |> Enum.max()
 res2 = Enum.to_list(min..max) -- all_passes
 IO.inspect(res2)
+
+#
+### Initial Approach
+#
+# def splitPassport(p) do
+    #     %{
+    #         "row" => locateDim(p |> String.slice(0..6)),
+    #         "col" => locateDim(p |> String.slice(7..10))
+    #     }
+    # end
+    # def locateDim(d) do
+    #     d |> String.replace(["F", "B", "L", "R"],
+    #         fn
+    #             z when z in ["F", "L"] -> "0"
+    #             o when o in ["B", "R"] -> "1"
+    #         end
+    #     )
+    # end
+    # def seatId(s) do
+    #     {c, _} = Integer.parse(s["col"], 2)
+    #     {r, _} = Integer.parse(s["row"], 2)
+    #     c + r * 8
+# end
